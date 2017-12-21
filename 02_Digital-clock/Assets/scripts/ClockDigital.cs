e300220cdecfd1af6e0930d4243a2c410b2df90c
Clair <acheron_1405@hotmail.com>
2017-12-21 17:44:16 -0600

    Repositorio actualizado hasta el último proyecto

---
+++ 02_Digital-clock/Assets/scripts/ClockDigital.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+using UnityEngine.UI;
+using System;
+
+public class ClockDigital : MonoBehaviour {
+
+    private Text textClock;
+
+	//Se inicializa el código
+	void Start () {
+        textClock = GetComponent<Text>();
+	}
+	
+	// Se actualiza una vez por frame
+	void Update () {
+        DateTime time = DateTime.Now;
+        String hour = LeadingZero(time.Hour);
+        String minute = LeadingZero(time.Minute);
+        String second = LeadingZero(time.Second);
+
+        textClock.text = "La hora es: "+hour + ":" + minute + ":" + second;
+	}
+
+    String LeadingZero(int n){
+        return n.ToString().PadLeft(2, '0');
+    }
+}
